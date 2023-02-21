3fcdb02ead4e7758369b8034a549e332c9db1f5e
Austin Mazur <mazuraustin1@gmail.com>
2023-02-21 22:20:23 +0100

    Initial brainstorm, working to understand the new concepts

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,275 @@
+
+
+
+// const movies = [
+//     {
+//       title: 'The Shawshank Redemption',
+//       year: 1994,
+//       director: 'Frank Darabont',
+//       duration: '2h 22min',
+//       genre: ['Crime', 'Drama'],
+//       score: 9.3
+//     },
+//     {
+//       title: 'The Godfather',
+//       year: 1972,
+//       director: 'Francis Ford Coppola',
+//       duration: '2h 55min',
+//       genre: ['Crime', 'Drama'],
+//       score: 9.2
+//     },
+//     {
+//       title: 'The Godfather: Part II',
+//       year: 1974,
+//       director: 'Francis Ford Coppola',
+//       duration: '3h 22min',
+//       genre: ['Crime', 'Drama'],
+//       score: 9
+//     },
+//     {
+//       title: 'The Dark Knight',
+//       year: 2008,
+//       director: 'Christopher Nolan',
+//       duration: '2h 32min',
+//       genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+//       score: 9
+//     },
+//     {
+//       title: '12 Angry Men',
+//       year: 1957,
+//       director: 'Sidney Lumet',
+//       duration: '1h 36min',
+//       genre: ['Crime', 'Drama'],
+//       score: 8.9
+//     },
+//     {
+//       title: 'Schindler"s List',
+//       year: 1993,
+//       director: 'Steven Spielberg',
+//       duration: '3h 15min',
+//       genre: ['Biography', 'Drama', 'History'],
+//       score: 8.9
+//     },
+//     {
+//       title: 'Pulp Fiction',
+//       year: 1994,
+//       director: 'Quentin Tarantino',
+//       duration: '2h 34min',
+//       genre: ['Crime', 'Drama'],
+//       score: 8.9
+//     },
+//     {
+//       title: 'The Lord of the Rings: The Return of the King',
+//       year: 2003,
+//       director: 'Peter Jackson',
+//       duration: '3h 21min',
+//       genre: ['Adventure', 'Drama', 'Fantasy'],
+//       score: 8.9
+//     },
+//     {
+//       title: 'Il buono, il brutto, il cattivo',
+//       year: 1966,
+//       director: 'Sergio Leone',
+//       duration: '3h 2min',
+//       genre: ['Western'],
+//       score: 8.9
+//     },
+//     {
+//       title: 'Fight Club',
+//       year: 1999,
+//       director: 'David Fincher',
+//       duration: '2h 19min',
+//       genre: ['Drama'],
+//       score: 8.8
+//     },
+//     {
+//       title: 'The Lord of the Rings: The Fellowship of the Ring',
+//       year: 2001,
+//       director: 'Peter Jackson',
+//       duration: '2h 58min',
+//       genre: ['Adventure', 'Drama', 'Fantasy'],
+//       score: 8.8
+//     },
+//     {
+//       title: 'Forrest Gump',
+//       year: 1994,
+//       director: 'Robert Zemeckis',
+//       duration: '2h 22min',
+//       genre: ['Comedy', 'Drama', 'Romance'],
+//       score: 8.8
+//     },
+//     {
+//       title: 'Star Wars: Episode V - The Empire Strikes Back',
+//       year: 1980,
+//       director: 'Irvin Kershner',
+//       duration: '2h 4min',
+//       genre: ['Action', 'Adventure', 'Fantasy', 'Sci-Fi'],
+//       score: 8.8
+//     },
+//     {
+//       title: 'Inception',
+//       year: 2010,
+//       director: 'Christopher Nolan',
+//       duration: '2h 28min',
+//       genre: ['Action', 'Adventure', 'Sci-Fi', 'Thriller'],
+//       score: 8.8
+//     },
+//     {
+//       title: 'The Lord of the Rings: The Two Towers',
+//       year: 2002,
+//       director: 'Peter Jackson',
+//       duration: '2h 59min',
+//       genre: ['Adventure', 'Drama', 'Fantasy'],
+//       score: 8.7
+//     },
+//     {
+//       title: 'One Flew Over the Cuckoo"s Nest',
+//       year: 1975,
+//       director: 'Milos Forman',
+//       duration: '2h 13min',
+//       genre: ['Drama'],
+//       score: 8.7
+//     },
+//     {
+//       title: 'Goodfellas',
+//       year: 1990,
+//       director: 'Martin Scorsese',
+//       duration: '2h 26min',
+//       genre: ['Crime', 'Drama'],
+//       score: 8.7
+//     },
+//     {
+//       title: 'The Matrix',
+//       year: 1999,
+//       director: 'Lana Wachowski',
+//       duration: '2h 16min',
+//       genre: ['Action', 'Sci-Fi'],
+//       score: 8.7
+//     },
+//     {
+//       title: 'Shichinin no samurai',
+//       year: 1954,
+//       director: 'Akira Kurosawa',
+//       duration: '3h 27min',
+//       genre: ['Adventure', 'Drama'],
+//       score: 8.7
+//     },
+//     {
+//       title: 'Star Wars',
+//       year: 1977,
+//       director: 'George Lucas',
+//       duration: '2h 1min',
+//       genre: ['Action', 'Adventure', 'Fantasy', 'Sci-Fi'],
+//       score: 8.7
+//     },
+//     {
+//       title: 'Cidade de Deus',
+//       year: 2002,
+//       director: 'Fernando Meirelles',
+//       duration: '2h 10min',
+//       genre: ['Crime', 'Drama'],
+//       score: 8.7
+//     },
+//     {
+//       title: 'Se7en',
+//       year: 1995,
+//       director: 'David Fincher',
+//       duration: '2h 7min',
+//       genre: ['Crime', 'Drama', 'Mystery', 'Thriller'],
+//       score: 8.6
+//     },
+//     {
+//       title: 'The Silence of the Lambs',
+//       year: 1991,
+//       director: 'Jonathan Demme',
+//       duration: '1h 58min',
+//       genre: ['Crime', 'Drama', 'Thriller'],
+//       score: 8.6
+//     },
+//     {
+//       title: 'It"s a Wonderful Life',
+//       year: 1946,
+//       director: 'Frank Capra',
+//       duration: '2h 10min',
+//       genre: ['Drama', 'Family', 'Fantasy'],
+//       score: 8.6
+//     },
+//     {
+//       title: 'La vita è bella',
+//       year: 1997,
+//       director: 'Roberto Benigni',
+//       duration: '1h 56min',
+//       genre: ['Comedy', 'Drama', 'War'],
+//       score: 8.6
+//     },
+//     {
+//       title: 'The Usual Suspects',
+//       year: 1995,
+//       director: 'Bryan Singer',
+//       duration: '1h 46min',
+//       genre: ['Crime', 'Drama', 'Mystery', 'Thriller'],
+//       score: 8.6
+//     }
+// ];
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+
+/*"TODO"
+- use the .map method to loop through movies array
+- return an array of all the directors
+*/
+
+/* "BUG 
+Brainfart right off the bat, unable to get a new array of directors
+was running something that was giving me undefined
+tried something else and ended up getting the values but jasmine tests failed
+*/
+function getAllDirectors(moviesArray) {
+    const allDirectors = moviesArray.map((director) => {
+        console.log(movies[0]);
+        return director
+        
+    }
+)}
+// getAllDirectors(movies)
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
-
+/* "TODO 
+- use .filter() method to loop 
+- use a conditional to see if Spielberg & if its a drama
+- return a number
+- return 0 if array is empty & if none are drama
+*/
+function howManyMovies(moviesArray) {
+    const drama = moviesArray.filter(function (movie) {
+        if(moviesArray.includes('Steven Spielberg') && moviesArray.includes('Drama')) {
+            console.log();
+            return drama;
+        }
+    })
+}
+// howManyMovies(movies)
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+
+/*"TODO 
+- use reduce to add up all the movie scores
+- get average then by diving the sum by moviesArray.length
+*/
+function scoresAverage(moviesArray) {
+    const avgRating = moviesArray.reduce((acc, curr) => {
+        console.log(acc + curr);
+        return acc + curr
+    })
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
+/* "TODO"
+- use .map() to get average score of all drama movies
+- use conditional to check if the movie is a drama
+- maybe create avgScore var 
+*/
 function dramaMoviesScore(moviesArray) {}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
